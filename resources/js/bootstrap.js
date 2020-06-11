ffb601077fe40c62b092713ead627cca7cd9336b
Gianfranco <email@gianfrancomossa.com>
2020-06-11 20:54:10 +0200

    Sprint 3: setting authHost for laravelEcho
    Signed-off-by: Gianfranco <email@gianfrancomossa.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,10 +35,11 @@ import Echo from "laravel-echo"
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
+    authHost: 'https://gianfrancomossa.com/teamup-url-anti-tony-dany/',
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsPort: 6080,
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
 });
